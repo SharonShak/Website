39f8b91c1fc1eeecee7ef02ae2e64bf94bc9938a
SharonShak <163827590+SharonShak@users.noreply.github.com>
2024-03-29 11:49:25 +0300

    Add files via upload

---
+++ Jewelry store/Product.js
@@ -0,0 +1,17 @@
+function Review1(Img)
+{
+    Img.src="Images/winston gates harry winston bracelet diamond yellow gold.webp";
+}
+function NoReview1(Img)
+{
+    Img.src="Images/winston gates harry winston bracelet diamond yellow gold.webp";
+}
+
+function Review2(Img)
+{
+    Img.src="Images/winston gates harry winston bracelet diamond platinum.webp";
+}
+function NoReview2(Img)
+{
+    Img.src="Images/winston gates harry winston bracelet diamond yellow gold.webp";
+}
